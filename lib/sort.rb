47d12dbeef7a1daec8d05e9173d721db3d93f46c
Joel Shooster <JBShooster@gmail.com>
2015-03-17 09:31:40 -0700

    Tests pass now in Rspec

--- lib/sort.rb
+++ lib/sort.rb
@@ -1,19 +1,25 @@
+require 'pry'
+
 class Array
 
-  def bubble_sort(array)
-    n = array.length
+  def bubble_sort
+    n = self.length
     loop do
       swapped = false
 
       (n-1).times do |i|
-        if array[i] > array[i+1]
-          array[i], array[i+1] = array[i+1], array [i]
+        if self[i] > self[i+1]
+          self[i], self[i+1] = self[i+1], self[i]
           swapped = true
         end
       end
       break if not swapped
     end
 
-  array
+  self
   end
 end
+
+a = Array.new [6,4,2,1,3]
+
+a.bubble_sort()
